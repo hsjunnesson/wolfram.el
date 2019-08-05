be7aad4825acc0712c9efa4b284c5e502f36fef5
Hans Sjunnesson <hans.sjunnesson@gmail.com>
2019-08-05 12:04:45 +0200

    Update wolfram.el
    Updated version.

--- wolfram.el
+++ wolfram.el
@@ -4,7 +4,7 @@
 
 ;; Author: Hans Sjunnesson <hans.sjunnesson@gmail.com>
 ;; Keywords: math
-;; Version: 1.1
+;; Version: 1.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
